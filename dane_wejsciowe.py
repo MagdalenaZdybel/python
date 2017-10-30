bc524fee56c59190f7c8cec966c620dda70b6f47
Magda <magda@motta.com.pl>
2017-10-30 10:39:48 +0100

    pobieranie plikow z internetu

---
+++ dane_wejsciowe.py
@@ -0,0 +1,11 @@
+print("Wprowadz swoje imię: ", )
+jakiesImie = input()
+print("Cześć,", jakiesImie, ". Jak Ci mija dzień?")
+
+print("mam", end=" ")
+print("na", end=" ")
+print("imię", end=" ")
+print("Dawid")
+
+jakiesImie = input("Wprowadź swoje imię: ")
+
--- konwertowanie_temperatury.py
+++ konwertowanie_temperatury.py
@@ -1,4 +1,10 @@
 from decimal import Decimal
-StopnieF = 65
+print("Program przeliczający temperaturę ze stopni Fahrenheita na stopnie Celsjusza")
+print("Wprowadz temperaturę w stopniach Fahrenheita: ")
+StopnieF = float(input())
 StopnieC = Decimal(float(5) / 9.0 * (StopnieF - 32))
 print(StopnieF, 'stopni w skali Fahrenheita: ', 'to', round(StopnieC, 2), 'stopni Celsusza')
+
+podajTemp = input()
+fahrenheita = (float(podajTemp))
+fahrenheita = (float(input("Poda")))
