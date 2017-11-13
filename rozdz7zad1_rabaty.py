f3b22ff71a78d7fed59dac3239c127d25ca8d3c4
Magda <magda@motta.com.pl>
2017-11-13 14:03:11 +0100

    kolejna akutalizacja

---
+++ rozdz7zad1_rabaty.py
@@ -0,0 +1,8 @@
+zakupy = float(input("Podaj kwotę za zakupy: "))
+rabat10 = zakupy * 0.10
+rabat20 = zakupy * 0.20
+
+if zakupy <= 50:
+    print("Rabat wynosi " , "10%" , ", kwota rabatu: " , rabat10, "do zapłaty: ", (zakupy - rabat10))
+else:
+    print("Rabat wynosi: " , "20%", ", kwota rabatu: ", rabat20 , "do zapłaty: ", (zakupy - rabat20))
---
+++ rozdz7zad2.py
@@ -0,0 +1,2 @@
+#drużyna piłkarska poszukuje  dziewczyn do zespołu. wiek od 10 do 12 lat. Pytanie o płeć ("d" dziewczynka, "c" chłopiec.
+#komunikat informujmący o tym, że dany użytkownik kwalifikuje się do drużyny. Dodatkowo - chłopcóww nie pyta już o wiek.
