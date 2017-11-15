f395e322375246c186695f44ac3e62d4f5f51d8c
Magda <magda@motta.com.pl>
2017-11-15 18:35:53 +0100

    kolejna aktualizacja calosci rozdz 8

--- tabMnozDlaDowLiczby.py
+++ tabMnozDlaDowLiczby.py
@@ -1,8 +1,9 @@
-print("wprowadz liczbę dla któej chcesz wyświetlić tabliczkę możenia:")
+print("wprowadz liczbę dla któej chcesz wyświetlić tabliczkę możenia: ")
 podanaLiczba = int(input())
+dlugoscLiczby = int(input("jak dluga ma byc tabliczka? "))
 print("Oto tabliczka: ")
 i = 1
-while i <= 10:
+while i <= dlugoscLiczby:
     print(podanaLiczba," x ", i, "=", podanaLiczba * i)
     i = i + 1
 
